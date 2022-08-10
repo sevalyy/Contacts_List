fda1ecc435a9860b32f6b49681499c27f1f6fecc
sevalyy <seval@yamanyar.com>
2022-08-10 14:30:46 +0300

    new contact,  seen in the list

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-function Form() {
-  const [form, setForm] = useState({ full_name: "", phone_number: "" });
+const initial = { full_name: "", phone_number: "" };
+
+// Contacts component'indan addContact'i buraya gonderdim.
+function Form({ setContacts, contacts }) {
+  const [form, setForm] = useState(initial);
+
+  useEffect(() => {
+    setForm(initial);
+  }, [contacts]);
 
   // formun her bir degerini alip e.target.name'ini e.target.value'suna esitliyorsun.
   const onChangeInput = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
+  // formun default davranisini engellemek icin e.preventDefault kullan.
   const onSubmit = (e) => {
     e.preventDefault();
     if (form.full_name === "" || form.phone_number === "") {
       return false;
     }
+    setContacts([...contacts, form]);
+    //fonk icinde de initial degerini set edebiliriz, useEffect icinde de
+    // setForm(initial);
 
+    // bir liste haline getirmek icin
+    // kontaklarin herbirini alip formdaki bilgileri bu kontaklara ekle.
     console.log(form);
   };
   return (
@@ -21,14 +34,16 @@ function Form() {
       <div>
         <input
           name="full_name"
-          placeholder="Full name"
+          placeholder="Full Name"
+          value={form.full_name}
           onChange={onChangeInput}
         ></input>
       </div>
       <div>
         <input
           name="phone_number"
-          placeholder="Phone number"
+          placeholder="Phone Number"
+          value={form.phone_number}
           onChange={onChangeInput}
         ></input>
       </div>
