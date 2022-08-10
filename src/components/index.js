ca15cc51cf3bf6c333ffa64732a65d1809cd75f4
sevalyy <seval@yamanyar.com>
2022-08-10 14:33:44 +0300

    set and track contacts with useEffect

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import List from "./Contacts/List";
 import Form from "./Contacts/Form";
 
 function Contacts() {
+  //iki component arasi iletisimi saglamak icin contact'i burada tutuyorum.
+
+  const [contacts, setContacts] = useState([
+    {
+      full_name: "Seval",
+      phone_number: 55555,
+    },
+    {
+      full_name: "Kaan",
+      phone_number: 66666,
+    },
+    {
+      full_name: "Bade",
+      phone_number: 33333,
+    },
+  ]);
+
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
   return (
     <div>
-      <List />
-      <Form />
+      <List contacts={contacts} />
+      <Form setContacts={setContacts} contacts={contacts} />
     </div>
   );
 }
