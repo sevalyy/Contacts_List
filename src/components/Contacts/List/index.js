406158ed684e1e6202fa6eebbf54c135c41f8598
sevalyy <seval@yamanyar.com>
2022-08-10 14:34:28 +0300

    Filter an array includes objects

--- src/components/Contacts/List/index.js
+++ src/components/Contacts/List/index.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-function List() {
-  return <div>Contact List</div>;
+function List({ contacts }) {
+  const [filterText, setFilterText] = useState("");
+
+  const filtered = contacts.filter((contact) => {
+    return Object.keys(contact).some((key) => {
+      return contact[key]
+        .toString()
+        .toLowerCase()
+        .includes(filterText.toLowerCase());
+    });
+  });
+  return (
+    <div>
+      <input
+        placeholder="Filter Contacts"
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      ></input>
+      <ul>
+        {filtered.map((contact, i) => (
+          <li key={i}>
+            {" "}
+            {contact.full_name}: {contact.phone_number}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default List;
